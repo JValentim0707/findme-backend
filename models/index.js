e577d3c5645f4b9fcbd09461917ae36b970631be
Jvalentim <jvalentim@imcba.com>
2024-07-23 10:32:19 -0300

    Config DB Conection

--- models/index.js
+++ models/index.js
@@ -7,25 +7,12 @@ import process from 'process';
 
 
 const basename = path.basename(__filename);
-const env = 'development';
+const env = 'devSystem';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-console.log('config.use_env_variable', config.use_env_variable)
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize({
-    host: config.host,
-    username: config.username,
-    password: config.password,
-    database: config.database,
-    dialect: config.dialect,
-    dialectModule: require('pg'),
-    benchmark: true
-  })
-}
+console.log('db', config)
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 fs
   .readdirSync(__dirname)
